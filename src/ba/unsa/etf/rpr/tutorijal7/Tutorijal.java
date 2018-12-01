08ff6c1386c4bb6e13b44dac7bace7b0e3b2f0a7
Lamija <lfazlija1@etf.unsa.ba>
2018-12-01 21:07:32 +0100

    dodana metoda ucitajGradove

--- src/ba/unsa/etf/rpr/tutorijal7/Tutorijal.java
+++ src/ba/unsa/etf/rpr/tutorijal7/Tutorijal.java
@@ -1,7 +1,22 @@
 package ba.unsa.etf.rpr.tutorijal7;
 
-public class Tutorijal {
+import java.io.FileNotFoundException;
+import java.io.FileReader;
+import java.util.ArrayList;
+import java.util.Scanner;
 
+public class Tutorijal {
+    ArrayList<Grad> ucitajGradove(){
+        Scanner ulaz;
+        try {
+            ulaz = new Scanner(new FileReader("mjerenje.txt"));
+        } catch (FileNotFoundException e) {
+            System.out.println("Datoteka mjerenje.txt ne postoji ili se ne može otvoriti");
+        }
+        ArrayList<Grad> listaGradova;
+
+
+    }
     public static void main(String[] args) {
 	// write your code here
     }
